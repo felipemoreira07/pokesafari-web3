2190d62628dbadc64c3e91c6cb3cac326dedf39a
felipemoreira07 <moreiraf242@gmail.com>
2024-12-18 16:36:00 -0300

    fix: change file name

--- contracts/Accountability.sol
+++ contracts/Accountability.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract AccountabilityContract {
+contract Accountability {
     struct Task {
         string description;
         bool isCompleted;
